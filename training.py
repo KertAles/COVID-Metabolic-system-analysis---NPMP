600b9d4b35453451a0c47d2b42cb5a6ae8ba1137
Neki Nekaj <ales.kert@gmail.com>
2022-12-25 23:07:42 +0100

    Added new training methods

---
+++ training.py
@@ -0,0 +1,81 @@
+import sklearn
+from sklearn import tree
+from sklearn.naive_bayes import GaussianNB
+import matplotlib.pyplot as plt
+
+from utils import read_split_data
+import pickle
+
+def evaluate_model(model, test_X, test_Y) :
+    #prediction = model.predict(test_X)
+    #acc = sklearn.metrics.accuracy_score(test_Y, prediction)
+    acc = model.score(test_X, test_Y)
+    return acc
+
+
+if __name__ == '__main__' :
+    results = {}
+    n_nb_reactions = 0
+    n_dt_reactions = 0
+    n_reactions = 0
+    for model in ['Gimme', 'iMAT', 'init', 'Tinit']:
+        for cell in ['293T', 'A549', 'CALU', 'Lung', 'NHBE']:
+            model_name = model + '_' + cell
+
+            print('Training on model ' + model_name)
+
+            train_X, train_Y, test_X, test_Y, reactions = read_split_data(model=model, cell=cell, shuffle=True, ratio=0.5)
+            n_reactions += len(reactions)
+
+            model_gauss = GaussianNB()
+            model_tree = tree.DecisionTreeClassifier()
+
+            model_gauss.fit(train_X, train_Y)
+            model_tree.fit(train_X, train_Y)
+
+            acc_nb = evaluate_model(model_gauss, test_X, test_Y)
+            acc_dt = evaluate_model(model_tree, test_X, test_Y)
+
+            results[model_name] = {'NB': acc_nb,
+                                   'DT': acc_dt}
+            print('Accuracy --- NB : ' + str(acc_nb) + '  DT : ' + str(acc_dt))
+
+            dt_reactions = []
+            nb_reactions = []
+
+            for i, reaction in enumerate(reactions) :
+
+                model_gauss.fit(train_X[:, i].reshape(-1, 1), train_Y)
+                model_tree.fit(train_X[:, i].reshape(-1, 1), train_Y)
+
+                if abs(model_gauss.var_[0]) > 1e-6 :
+                    acc_nb = evaluate_model(model_gauss, test_X[:, i].reshape(-1, 1), test_Y)
+                    acc_dt = evaluate_model(model_tree, test_X[:, i].reshape(-1, 1), test_Y)
+                    #print('Accuracy for reaction ' + reaction + ' --- NB : ' + str(acc_nb) + '  DT : ' + str(acc_dt))
+                    if acc_nb == 1.0 :
+                        nb_reactions.append(reaction)
+                    if acc_dt == 1.0 :
+                        dt_reactions.append(reaction)
+
+            #tree.plot_tree(model_tree)
+            #plt.show()
+
+            n_nb_reactions += len(nb_reactions)
+            n_dt_reactions += len(dt_reactions)
+
+            f = open('./models/' + model_name + '_NB' + '.pickle', 'wb')
+            pickle.dump(model_gauss, f)
+            f.close()
+
+            f = open('./models/' + model_name + '_DT' + '.pickle', 'wb')
+            pickle.dump(model_tree, f)
+            f.close()
+
+    n_nb_reactions /= 20
+    n_dt_reactions /= 20
+    n_reactions /= 20
+    print('Average number of common reactions : ' + str(n_reactions))
+    print('Average changed reactions --- NB : ' + str(n_nb_reactions) + '  DT : ' + str(n_dt_reactions))
+    print(results)
+
+    print('Done.')
--- utils.py
+++ utils.py
@@ -1,30 +1,42 @@
 import pandas as pd
 import numpy as np
 
-def read_data(path='./data/', model='Gimme', cells='293T') :
+def read_data(path='./data/', model='Gimme', cell='293T', shuffle=False, shuffle_seed=42) :
     # Construct base path
-    base_path = path + model + '_' + cells + '_sample_' 
+    base_path = path + model + '_' + cell + '_sample_'
     # Read healthy and infected cells data
     data_H = pd.read_csv(base_path + 'H.csv', sep = ';')
     data_I = pd.read_csv(base_path + 'I.csv', sep = ';')
+
+    if shuffle :
+        data_H = data_H.sample(frac=1, random_state=shuffle_seed)
+        data_I = data_I.sample(frac=1, random_state=shuffle_seed)
+
     # Extract common columns
     common_reactions = sorted(list(set(data_H.columns) & set(data_I.columns)))
     data_H = data_H[common_reactions]
     data_I = data_I[common_reactions]
-    return (data_H, data_I)
-
+    return (data_H, data_I), common_reactions
 
 def split_data(data, ratio=0.2) :
     # Data oblike (data_H, data_I)
     X = np.concatenate((data[0], data[1]), axis=0)
     Y = np.concatenate((np.zeros(1000),  np.ones(1000)), axis=0)
+
     start = int(ratio * 1000)
     end = 2000 - int(ratio * 1000)
-    train_set_X = X[start:end, :]
-    train_set_Y = Y[start:end]
-    test_set_X = np.concatenate((X[0:start, :], X[end:, :]))
-    test_set_Y = np.concatenate((Y[0:start], Y[end:]))
-    return {"X":[train_set_X, test_set_X], "Y":[train_set_Y, test_set_Y]}
+    train_X = X[start:end, :]
+    train_Y = Y[start:end]
+    test_X = np.concatenate((X[0:start, :], X[end:, :]))
+    test_Y = np.concatenate((Y[0:start], Y[end:]))
+
+    return train_X, train_Y, test_X, test_Y
+
+def read_split_data(path='./data/', model='Gimme', cell='293T', shuffle=False, shuffle_seed=42, ratio=0.2) :
+    data, reactions = read_data(path, model, cell, shuffle, shuffle_seed)
+    train_X, train_Y, test_X, test_Y = split_data(data, ratio)
+
+    return train_X, train_Y, test_X, test_Y, reactions
 
 def crossevaluation(data, i=0, ratio=0.2) :
     # TODO:: Za na koncu
