02ede4de8ea70bcafbfef9edb02dcafff738674b
Neki Nekaj <ales.kert@gmail.com>
2023-01-08 22:54:09 +0100

    dunno what i changed

--- training.py
+++ training.py
@@ -20,56 +20,58 @@ if __name__ == '__main__' :
     n_reactions = 0
     for model in ['Gimme', 'iMAT', 'init', 'Tinit']:
         for cell in ['293T', 'A549', 'CALU', 'Lung', 'NHBE']:
-            model_name = model + '_' + cell
+            # TODO :: preberi podatke za model in cell - s subsystemi
+            # for  subsys in subsystems :
+                model_name = model + '_' + cell
 
-            print('Training on model ' + model_name)
+                print('Training on model ' + model_name)
 
-            train_X, train_Y, test_X, test_Y, reactions = read_split_data(model=model, cell=cell, shuffle=True, ratio=0.5)
-            n_reactions += len(reactions)
+                train_X, train_Y, test_X, test_Y, reactions = read_split_data(model=model, cell=cell, shuffle=True, ratio=0.5)
+                n_reactions += len(reactions)
 
-            model_gauss = GaussianNB()
-            model_tree = tree.DecisionTreeClassifier()
+                model_gauss = GaussianNB()
+                model_tree = tree.DecisionTreeClassifier()
 
-            model_gauss.fit(train_X, train_Y)
-            model_tree.fit(train_X, train_Y)
+                model_gauss.fit(train_X, train_Y)
+                model_tree.fit(train_X, train_Y)
 
-            acc_nb = evaluate_model(model_gauss, test_X, test_Y)
-            acc_dt = evaluate_model(model_tree, test_X, test_Y)
+                acc_nb = evaluate_model(model_gauss, test_X, test_Y)
+                acc_dt = evaluate_model(model_tree, test_X, test_Y)
 
-            results[model_name] = {'NB': acc_nb,
-                                   'DT': acc_dt}
-            print('Accuracy --- NB : ' + str(acc_nb) + '  DT : ' + str(acc_dt))
+                results[model_name] = {'NB': acc_nb,
+                                       'DT': acc_dt}
+                print('Accuracy --- NB : ' + str(acc_nb) + '  DT : ' + str(acc_dt))
 
-            dt_reactions = []
-            nb_reactions = []
+                dt_reactions = []
+                nb_reactions = []
 
-            for i, reaction in enumerate(reactions) :
+                for i, reaction in enumerate(reactions) :
 
-                model_gauss.fit(train_X[:, i].reshape(-1, 1), train_Y)
-                model_tree.fit(train_X[:, i].reshape(-1, 1), train_Y)
+                    model_gauss.fit(train_X[:, i].reshape(-1, 1), train_Y)
+                    model_tree.fit(train_X[:, i].reshape(-1, 1), train_Y)
 
-                if abs(model_gauss.var_[0]) > 1e-6 :
-                    acc_nb = evaluate_model(model_gauss, test_X[:, i].reshape(-1, 1), test_Y)
-                    acc_dt = evaluate_model(model_tree, test_X[:, i].reshape(-1, 1), test_Y)
-                    #print('Accuracy for reaction ' + reaction + ' --- NB : ' + str(acc_nb) + '  DT : ' + str(acc_dt))
-                    if acc_nb == 1.0 :
-                        nb_reactions.append(reaction)
-                    if acc_dt == 1.0 :
-                        dt_reactions.append(reaction)
+                    if abs(model_gauss.var_[0]) > 1e-6 :
+                        acc_nb = evaluate_model(model_gauss, test_X[:, i].reshape(-1, 1), test_Y)
+                        acc_dt = evaluate_model(model_tree, test_X[:, i].reshape(-1, 1), test_Y)
+                        #print('Accuracy for reaction ' + reaction + ' --- NB : ' + str(acc_nb) + '  DT : ' + str(acc_dt))
+                        if acc_nb == 1.0 :
+                            nb_reactions.append(reaction)
+                        if acc_dt == 1.0 :
+                            dt_reactions.append(reaction)
 
-            #tree.plot_tree(model_tree)
-            #plt.show()
+                #tree.plot_tree(model_tree)
+                #plt.show()
 
-            n_nb_reactions += len(nb_reactions)
-            n_dt_reactions += len(dt_reactions)
+                n_nb_reactions += len(nb_reactions)
+                n_dt_reactions += len(dt_reactions)
 
-            f = open('./models/' + model_name + '_NB' + '.pickle', 'wb')
-            pickle.dump(model_gauss, f)
-            f.close()
+                f = open('./models/' + model_name + '_NB' + '.pickle', 'wb')
+                pickle.dump(model_gauss, f)
+                f.close()
 
-            f = open('./models/' + model_name + '_DT' + '.pickle', 'wb')
-            pickle.dump(model_tree, f)
-            f.close()
+                f = open('./models/' + model_name + '_DT' + '.pickle', 'wb')
+                pickle.dump(model_tree, f)
+                f.close()
 
     n_nb_reactions /= 20
     n_dt_reactions /= 20
